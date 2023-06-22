4c4a948d4498e0bcb523b236d1d14cd18c7142d2
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-06-22 15:14:01 +0900

    Update 물타기.py

--- 물타기.py
+++ 물타기.py
@@ -6,15 +6,15 @@ with c1 :
             평단 = int(st.text_input('평균단가',0))
             수량 = int(st.text_input('보유수량',0))
             보유금액 = st.text_input('보유금액',f"{평단*수량:,}",disabled=True)
-            현재 = int(st.text_input('현재단가',0))
+            현재 = int(st.text_input('현재단가',0))                        
+            수익률 = (현재 - 평단) / 평단 * 100
+            st.warning(f"{평단:,d} ({round(수익률,2):+,}%)")
             매수금액 = 평단 * 수량
             추매수량 = st.number_input('추매수량',min_value=0,step=1)
             보유금액 = st.text_input('추매금액',f"{현재*추매수량:,}",disabled=True)
             추매 = 현재 * 추매수량
             물타기 = (매수금액 + 추매) / (수량 + 추매수량)
-            수익률 = (현재 - 평단) / 평단 * 100
             추매수익률 = (현재 - 물타기) / 물타기 * 100
-            st.warning(f"{평단:,d} ({round(수익률,2):+,}%)")
             if 수익률 <= 0 :
                 st.info(f"{int(물타기):,d} ({round(추매수익률,2):+,}%)")
             else:

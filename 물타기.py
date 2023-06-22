63e9f12cf98ed3d1e37047884510b05dbbe900ff
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-06-22 13:57:29 +0900

    Update 물타기.py

--- 물타기.py
+++ 물타기.py
@@ -5,11 +5,11 @@ with c1 :
         try:
             평단 = st.number_input('평균금액',min_value=0,step=50)
             수량 = st.number_input('보유수량',min_value=0,step=10)
-            st.write(평단*수량)
+            보유금액 = st.text_input('보유금액',f"{평단*수량:,}")
             현재 = st.number_input('현재금액',min_value=0,step=50)
             매수금액 = 평단 * 수량
             추매수량 = st.number_input("추매수량",min_value=0, step=10)
-            st.write(현재*추매수량)
+            보유금액 = st.text_input('추매금액',f"{추매수량*추매수량:,}")
             추매 = 현재 * 추매수량
             물타기 = (매수금액 + 추매) / (수량 + 추매수량)
             수익률 = (현재 - 평단) / 평단 * 100
@@ -19,6 +19,5 @@ with c1 :
                 st.info(f"{int(물타기):,d} ({round(추매수익률,2):+,}%)")
             else:
                 st.error(f"{int(물타기):,d} ({round(추매수익률,2):+,}%)")
-
         except ZeroDivisionError:
             st.warning('조건입력')
